ee25ad99422803f24890add3cc32d90e752013f3
dgukan <dogukankoc2507@gmail.com>
2024-02-02 20:11:29 +0300

    Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { data } from './data';
+import Navigation from './Navigation';
+import Body from './Body';
+import MyComponent from './myComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div id="top">
+                <main id="main">
+                    <header id="main-header">
+                        <a href="#top" id="logo">
+                            <h3>
+                                {data.content.logo}
+                            </h3>
+                        </a>
+                        <Navigation/>
+                        <footer id="main-header__footer">
+                            <ul id="main-header__footer_ul">
+                                {
+                                    data.socialMediaIcons.map((socialMediaIcon) => (
+                                        <li>
+                                            <a href={socialMediaIcon.href} target={socialMediaIcon.target}>
+                                                <FontAwesomeIcon icon={socialMediaIcon.icon}/>
+                                            </a>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </footer>
+                    </header>
+                   <Body data={data}/>
+                   Egzersiz:
+                   <MyComponent sayilar={[1, 2, 3, 4, 5]}/>
+                </main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
